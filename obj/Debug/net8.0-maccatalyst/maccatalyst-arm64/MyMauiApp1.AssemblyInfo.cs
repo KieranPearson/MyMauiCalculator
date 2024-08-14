ed6a902cc2389caaf03bf14dc914a742c9e22f09
Kieran Pearson <61248872+KieranPearson@users.noreply.github.com>
2024-08-14 12:10:28 +0100

    Added gitignore file

--- obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MyMauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MyMauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4510da128039a859055cb2548713749cf0b8584d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5959fd51219ac73f908228f6a97574f03b118640")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
