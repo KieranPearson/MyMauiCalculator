2a4eca01e634633fa5cff3685990ce730c71c2e9
Kieran Pearson <61248872+KieranPearson@users.noreply.github.com>
2024-08-15 10:44:51 +0100

    Removed top bar for Mac version

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using UIKit;
+
 namespace MyMauiApp1;
 
 public partial class App : Application
@@ -10,12 +11,19 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
+#if WINDOWS
 		MainPage = new AppShell();
+#endif
 	}
 
+#if MACCATALYST
     protected override Window CreateWindow(IActivationState? activationState)
     {
-		var window = base.CreateWindow(activationState);
+		var window = new Window(new AppShell());
+
+		window.HandlerChanged += WindowHandlerChanged;
+		var rootPage = new MainPage();
+		window.Page = rootPage;
 
 		window.MinimumWidth = WINDOW_WIDTH_PIXELS;
 		window.MaximumWidth = WINDOW_WIDTH_PIXELS;
@@ -34,4 +42,24 @@ public partial class App : Application
 
         return window;
     }
+
+
+	private void WindowHandlerChanged(object sender, EventArgs e)
+	{
+		var win = sender as Microsoft.Maui.Controls.Window;
+		try
+		{
+			var uiWin = win.Handler.PlatformView as UIWindow;
+
+			if (uiWin != null)
+			{
+				uiWin.WindowScene.Titlebar.TitleVisibility = UITitlebarTitleVisibility.Hidden;
+			}
+		}
+		catch (NullReferenceException ex)
+		{
+			Console.WriteLine("Error - UIWindow was null");
+		}
+	}
+#endif
 }
